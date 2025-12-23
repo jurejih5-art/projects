c176a698a7cc972a782d4b3771006a7b85c44e23
jurejih5-art <jurejih5@gmail.com>
2025-12-23 19:23:36 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,3 +7,10 @@ document.querySelectorAll('nav ul li a').forEach(link => {
         });
     });
 });
+
+// Mobile menu
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+menuBtn.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
