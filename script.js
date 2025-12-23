65497229db7968b65662476c0cf6993f1cc929f6
jurejih5-art <jurejih5@gmail.com>
2025-12-23 19:32:19 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -14,3 +14,14 @@ const navLinks = document.getElementById("nav-links");
 menuBtn.addEventListener("click", () => {
   navLinks.classList.toggle("show");
 });
+
+// Scroll animation for services
+const cards = document.querySelectorAll(".service-card");
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+}, { threshold: 0.2 });
